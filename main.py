406e6222b3ad916d293078bda3008e3f21f986bf
Terry <teraprath.thuansuriya@gmail.com>
2021-10-11 13:16:26 +0200

    Added 404
    + 404 Error Page

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def delete(id):
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return "Page Not Found LOL", 404
+    return render_template("404.html"), 404
 
 if __name__ == "__main__":
     app.run(port=5500, debug=True)
